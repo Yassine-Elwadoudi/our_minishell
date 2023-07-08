ce007af1411d490d35456cd1b7719515b3f9b5cb
yassineelwadoudi <yassineelwadoudi2@gmail.com>
2023-07-08 14:57:22 +0100

    fixed echo

--- src/built_ins/echo.c
+++ src/built_ins/echo.c
@@ -6,7 +6,7 @@
 /*   By: yelwadou <yelwadou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 13:32:41 by yelwadou          #+#    #+#             */
-/*   Updated: 2023/07/08 13:33:05 by yelwadou         ###   ########.fr       */
+/*   Updated: 2023/07/08 14:55:48 by yelwadou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,26 @@ static int only_n(char *str)
     return (1);
 }
 
-void	echo(int argc, char **argv)
+void echo(int argc, char **argv)
 {
-	int	i;
-	int display_return ;
+    int i;
+    int display_return;
 
-	display_return = 1;
-	i = 1;
-	if (argc > 1 && argv[1][0] == '-' && only_n(argv[1] + 2))
-	{
-		display_return = 0;
-		i++;
-	}
-	while (i < argc)
-	{
-		ft_putstr_fd(argv[i], STDOUT_FILENO);
-		if (i < argc - 1)
-			ft_putstr_fd(" ", STDOUT_FILENO);	// this adds spaces after each word till it reaches the end
-		i++;
-	}
-	if (display_return == 1)
-		ft_putstr_fd("\n", STDOUT_FILENO);
+    display_return = 1;
+    i = 1;
+    if (argc > 1 && argv[1][0] == '-' && argv[1][1] == 'n'
+		&& only_n(argv[1] + 2))
+    {
+        display_return = 0;
+        i++;
+    }
+    while (i < argc)
+    {
+        ft_putstr_fd(argv[i], STDOUT_FILENO);
+        if (i < argc - 1)
+            ft_putstr_fd(" ", STDOUT_FILENO);
+        i++;
+    }
+    if (display_return == 1)
+        ft_putstr_fd("\n", STDOUT_FILENO);
 }
